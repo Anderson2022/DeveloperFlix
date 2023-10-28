603d106f03c9adb56c3f37a4b8f8be972da7455d
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2023-10-28 19:37:17 -0400

    feat: get videos

--- src/components/LayoutPage.vue
+++ src/components/LayoutPage.vue
@@ -99,7 +99,9 @@ import DF from '@/assets/DF.png';
             </li>
             <li>
               <a href="/Videos"
-                class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white  md:p-0 md:dark:hover:text-white  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Videos</a>
+                class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white  md:p-0 md:dark:hover:text-white  dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" 
+                aria-current="page">
+                Videos</a>
             </li>
             <li>
               <a href="/Channels"
