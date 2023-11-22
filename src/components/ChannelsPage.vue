d9e81ddcbcd304b5e7e09dd9c080bce16b8a3571
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2023-11-22 16:38:11 -0400

    feat:  channels  page

--- src/components/ChannelsPage.vue
+++ src/components/ChannelsPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Section from '../components/Section.vue'
+import SectionChannelsPage from '../components/SectionChannelsPage.vue'
 import Carousel from '../components/Carousel .vue'
 import VideoModal from '@/components/VideoModal.vue';
 import DF from '@/assets/DF.png';
@@ -7,6 +7,7 @@ import DF from '@/assets/DF.png';
 import { ref } from 'vue';
 const showModal = ref(false);
 const Channels = 'Canais'
+const title = 'Canais'
 const openModal = () => {
   showModal.value = true;
 };
@@ -22,30 +23,43 @@ const closeModal = () => {
     <div class="text-white mx-auto w-full min-h-screen  h-auto flex flex-col ">
       <Carousel />
       <div class="flex flex-col relative mt-28 w-96 h-96 ml-12">
-        <div class="w-full h-80">
+        <div class="w-full h-52">
         </div>
         <div class="flex flex-col ">
-        <div class="flex items-center">
-      <img class="w-10 h-10 rounded-full mr-2" :src="DF" />
-      <h1 class="text-cyan-50 text-2xl font-bold">{{ Channels }}</h1>
-    </div>
+          <div class="flex items-center">
+            <img class="w-10 h-10 rounded-full mr-2" :src="DF" />
+            <h1 class="text-cyan-50 text-2xl font-bold">{{ Channels }}</h1>
+          </div>
           <div class="px-8">
             <header>
               <div class="">
-<p>teste</p>
+                <h1 class="text-6xl font-bold">{{ title }}</h1>
               </div>
             </header>
           </div>
+          <div class="flex">
+            <button
+              class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 w-32 rounded inline-flex items-center m-4">
+              <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3l14 9-14 9V3z"></path>
+              </svg>
+              Assistir
+            </button>
+
+            <button
+              class="bg-slate-500 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center m-4">
+              Mais Informações
+              <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+              </svg>
+            </button>
+          </div>
         </div>
-
-
-
-
-
       </div>
     </div>
     <div>
-      <Section />
+      <SectionChannelsPage />
     </div>
-  </div>
-</template>
+</div></template>
---
+++ src/components/SectionChannelsPage.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { defineProps, ref, onMounted } from 'vue'
+import { Carousel, Navigation, Slide } from 'vue3-carousel'
+import { getVideosCollection } from '../Api/Request/firebaseQueries';
+import { insertData } from '../Api/Model/insertData';
+
+import 'vue3-carousel/dist/carousel.css'
+
+const firstSectionVideos = ref();
+const secondSectionVideos = ref();
+const thirdSectionVideos = ref();
+
+const settings = {
+  itemsToShow: 1,
+  snapAlign: 'center',
+};
+
+const breakpoints = {
+  // 700px and up
+  700: {
+    itemsToShow: 3.5,
+    snapAlign: 'center',
+  },
+  // 1024 and up
+  1024: {
+    itemsToShow: 7,
+    snapAlign: 'start',
+  },
+};
+
+onMounted(async () => {
+  await searchVideos();
+});
+
+async function searchVideos() {
+  try {
+    const videos = await getVideosCollection();
+    firstSectionVideos.value = videos.slice(0, 15);
+    secondSectionVideos.value = videos.slice(10, 20);
+    thirdSectionVideos.value = videos.slice(20, 30);
+  } catch (error) {
+    console.error("Erro na pesquisa de vídeos:", error);
+  }
+}
+
+function inserirDados() {
+  insertData();
+}
+</script>
+
+<template>
+  <div class="">  
+  <section class="bg-black shadow  ">
+    <h3 class="text-gray-200 mx-8 pt-6 text-xl">JavaScript</h3>
+    <Carousel :settings="settings" :breakpoints="breakpoints" class="w-full">
+
+      <Slide v-for="video in firstSectionVideos" :key="video.videoId"
+        class="inline-block mr-1 cursor-pointer transform transition-transform duration-450 origin-center-left hover:opacity-100 hover:scale-150 w-48 z-100">
+        <a v-if="video.videoId" :href="'https://www.youtube.com/watch?v=' + video.videoId" target="_blank" class="w-52">
+          <img :src="video.URL" alt="Thumbnail" class="w-52" />
+        </a>
+      </Slide>
+      <template #addons>
+        <Navigation class="custom-navigation" />
+      </template>
+    </Carousel>
+  </section>
+  <section class="bg-black shadow">
+    <h3 class="text-gray-200 mx-8 pt-6 text-xl">PHP</h3>
+    <Carousel :settings="settings" :breakpoints="breakpoints" class="w-full">
+      <Slide v-for="video in secondSectionVideos" :key="video.videoId"
+        class="inline-block mr-1 cursor-pointer transform transition-transform duration-450 origin-center-left hover:opacity-100 hover:scale-150 w-48 z-100">
+        <a v-if="video.videoId" :href="'https://www.youtube.com/watch?v=' + video.videoId" target="_blank" class="w-52">
+          <img :src="video.URL" alt="Thumbnail" class="w-52" />
+        </a>
+      </Slide>
+      <template #addons>
+        <Navigation />
+      </template>
+    </Carousel>
+  </section>
+  <section class="bg-black shadow">
+    <h3 class="text-gray-200 mx-8 pt-6 text-xl">Python</h3>
+    <Carousel :settings="settings" :breakpoints="breakpoints" class="w-full">
+      <Slide v-for="video in thirdSectionVideos" :key="video.videoId"
+        class="inline-block mr-1 cursor-pointer transform transition-transform duration-450 origin-center-left hover:opacity-100 hover:scale-150 w-48 z-100">
+        <a v-if="video.videoId" :href="'https://www.youtube.com/watch?v=' + video.videoId" target="_blank" class="w-52">
+          <img :src="video.URL" alt="Thumbnail" class="w-52" />
+        </a>
+      </Slide>
+      <template #addons>
+        <Navigation />
+      </template>
+    </Carousel>
+  </section>
+  </div>
+</template>
+
+
